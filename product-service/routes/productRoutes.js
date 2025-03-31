a3288de4b97d47779f53b14499a63dc53c012440
Vinali Samarasinghe <it21321368@my.sliit.lk>
2025-03-31 11:30:47 +0530

    Add JWT authorization middleware

--- product-service/routes/productRoutes.js
+++ product-service/routes/productRoutes.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productController");
+const authMiddleware = require("../middleware/authMiddleware");
 
 
 router.get("/search", productController.searchProducts);
 router.get("/categories", productController.getCategories);
 router.get("/", productController.getAllProducts);
 router.get("/:id", productController.getProductById);
-router.post("/", productController.addProduct);  
-router.put("/:id", productController.updateProduct); 
-router.delete("/:id", productController.deleteProduct);
+
+router.post("/", authMiddleware, productController.addProduct);  
+router.put("/:id", authMiddleware, productController.updateProduct); 
+router.delete("/:id", authMiddleware, productController.deleteProduct);
 
 module.exports = router;

55fd68c8962d194f565684332db2db36b0ca67b6
Chamaththa123 <shamodchamaththa@gmail.com>
2025-05-16 11:39:36 +0530

    Update productCategoryRoutes.js

--- product-category-service/routes/productCategoryRoutes.js
+++ product-category-service/routes/productCategoryRoutes.js
@@ -4,7 +4,7 @@ const productCategoryController = require("../controllers/productCategoryControl
 const authMiddleware = require("../middleware/authMiddleware");
 
 
-router.get("/", authMiddleware,productCategoryController.getAllProductCategory);
+router.get("/", authMiddleware,productCategoryController.getAllProductCategory); 
 router.get("/:id",authMiddleware, productCategoryController.getProductCategoryById);
 router.post("/", authMiddleware, productCategoryController.addProductCategory);  
 router.put("/:id", authMiddleware, productCategoryController.updateProductCategory); 
